fec7e2356265c26a0348f4a3384f9fce472ed147
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-10-26 08:40:00 +0630

    feat: add basic middleware

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
-const express = require('express')
-
+const express = require("express");
 const app = express();
-
 const port = 5000;
 
-console.log('abc')
+// app level middleware
+app.use(express.json());
+
+// route level middleware
+app.get("/", (req, res) => {
+  console.log(req.method, req.url);
+  res.send("Home");
+});
+
+app.post("/about", (req, res) => {
+  console.log(req.method, req.url);
+  res.send("About");
+});
+
+// error handling middleware
+app.use((req, res, next, err) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+// third party middleware
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
