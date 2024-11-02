a94fbc10e79e32146a7f79f86132f853fbef20d5
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-11-02 19:22:30 +0630

    feat: login user

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,7 @@ const { ObjectId } = require("mongodb");
 const { getDB } = require("../db/db");
 const { BadRequest, NotFound } = require("../utils/AppError");
 const { tryCatch } = require("../utils/tryCatch");
+const bcrypt = require("bcrypt");
 
 exports.register = tryCatch(async (req, res, next) => {
   const db = getDB();
@@ -15,12 +16,28 @@ exports.register = tryCatch(async (req, res, next) => {
   if (password !== password_confirmation) {
     throw new BadRequest("Password does not match");
   }
+  const hashedPassword = bcrypt.hashSync(password, 10);
   const newUser = {
     name,
     email,
-    password,
-    password_confirmation,
+    password: hashedPassword,
   };
   const result = await collection.insertOne(newUser);
   res.status(201).json({ message: "Register Successfully", data: result });
+});
+
+exports.login = tryCatch(async (req, res, next) => {
+  const db = getDB();
+  const collection = db.collection("users");
+
+  const { email, password } = req.body;
+  const user = await collection.findOne({ email });
+  if (!user) {
+    throw new NotFound("User not found");
+  }
+  const isPasswordValid = bcrypt.compareSync(password, user.password);
+  if (!isPasswordValid) {
+    throw new BadRequest("Invalid credentials");
+  }
+  res.status(200).json({ message: "Login Successfully", data: user });
 });
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const { register } = require("../controller/userController");
+const { register, login } = require("../controller/userController");
 
 const userRouter = Router();
 
-userRouter.route("/").post(register);
+userRouter.route("/register").post(register);
+userRouter.route('/login').post(login)
 
 module.exports = userRouter;
